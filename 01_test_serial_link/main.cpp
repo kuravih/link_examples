676baf2408887704480a46e619122da5a5d19bea
Kuravi H <kuravih@gmail.com>
2025-12-24 17:30:20 -0500

    Adopt meson

--- 01_test_serial_link/main.cpp
+++ 01_test_serial_link/main.cpp
@@ -1,11 +1,6 @@
 #include "kato/log.hpp"
 #include "link/serial_link.hpp"
 
-inline void onError()
-{
-    kato::log::cout << KATO_RED << "Error" << KATO_RESET << std::endl;
-}
-
 int main()
 {
     SerialLink link("/dev/ttyGS0", B115200, onError);
